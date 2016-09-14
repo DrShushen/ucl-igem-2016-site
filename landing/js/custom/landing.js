582aa97c9d5a8b842977161fac066eacaff77ebe
DrShushen <e.s.saveliev@gmail.com>
2016-09-14 05:23:44 +0100

    * Landing page set up.

--- landing/js/custom/landing.js
+++ landing/js/custom/landing.js
@@ -1,5 +1,17 @@
-$(document).ready(function() {
+$(document).ready(function($) {
+    var menuLandingHeaderHeight = 80; // HAS A COUNTERPART IN _menu-landing-variables.scss, MUST KEEP SYNCHRONISED!
     $('#fullpage').fullpage({
-        anchors:['firstPage', 'secondPage', 'thirdPage']
+        css3: 'true',
+        //anchors:['firstPage', 'secondPage', 'thirdPage', 'fourthPage'],
+        //fixedElements: '#landing-header',
+        menu: '#cd-primary-nav',
+        paddingTop: menuLandingHeaderHeight,
+        continuousHorizontal: true,
+        slidesNavigation: true,
+        scrollOverflow: true,
+        scrollOverflowOptions: {
+            mouseWheelSpeed: 3.5,
+            shrinkScrollbars: true
+        }
     });
 });
