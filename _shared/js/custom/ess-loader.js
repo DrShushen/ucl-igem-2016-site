69581151eba5384b005880532e67d31f007ce73d
DrShushen <e.s.saveliev@gmail.com>
2016-09-16 14:34:35 +0100

    * Page not reloading properly on links issue fixed.

--- _shared/js/custom/ess-loader.js
+++ _shared/js/custom/ess-loader.js
@@ -1,7 +1,6 @@
 function onReady_UCLiGem(callback) {
     var intervalID = window.setInterval(checkReady, 300);
     function checkReady() {
-        console.log("Checking Ready");
         if (document.getElementsByTagName('body')[0] !== undefined && document.getElementById('body-loaded') !== null) {
             window.clearInterval(intervalID);
             callback.call(this);
@@ -10,7 +9,6 @@ function onReady_UCLiGem(callback) {
 }
 
 function show_UCLiGem(id, value) {
-    console.log("Show: " + id);
     if (document.getElementById(id) !== null && document.getElementById(id).style !== null) {
         document.getElementById(id).style.display = value ? 'block' : 'none';
     }
